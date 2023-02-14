5fd8e4b3de153eb3a74b29efc935ca90c3e46236
Денис Максимук <59338803+DenysDevelopment@users.noreply.github.com>
2023-02-14 15:38:12 +0000

    fef

--- script.js
+++ script.js
@@ -8,6 +8,13 @@ document.write(tg.initDataUnsafe.user.first_name);
 tg.MainButton.textColor = "#ffffff";
 tg.MainButton.color = "#ffffff";
 
+tg.MainButton.text = "Changed Text"; //изменяем текст кнопки
+tg.MainButton.setText("Changed Text1"); //изменяем текст кнопки иначе
+tg.MainButton.textColor = "#F55353"; //изменяем цвет текста кнопки
+tg.MainButton.color = "#143F6B"; //изменяем цвет бэкграунда кнопки
+tg.MainButton.setParams({ "color": "#143F6B" }); //так изменяются все параметры
+
+
 tg.sendData(
 	JSON.stringify({
 		phone: document.querySelector("#phone").value,
