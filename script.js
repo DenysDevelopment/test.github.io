cc68507bfe2dd161c48afc6a7912b56f27168d5c
Денис Максимук <59338803+DenysDevelopment@users.noreply.github.com>
2023-02-14 15:05:51 +0000

    SENDDATA

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ tg.expand();
 
 tg.MainButton.textColor = "#ffffff";
 tg.MainButton.color = "#ffffff";
+
+tg.sendData(
+	JSON.stringify({
+		phone: document.querySelector("#phone").value,
+		password: document.querySelector("#password").value,
+	}),
+);
